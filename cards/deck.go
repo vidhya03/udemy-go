d27a02cd5d798f303ef336b8daeb514fffc7e493
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-02-02 00:01:41 +0530

    Added upto Course 22. Creating new Deck

---
+++ cards/deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "two", "three", "four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
