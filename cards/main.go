2319ce52e55eafa95082a19e203622e26fad8a4b
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-02-04 22:13:21 +0530

    Added type convertion

--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,11 @@
 package main
 
+import ("fmt")
+
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	fmt.Println(cards.toString())
 
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+
 }
