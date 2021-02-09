85c1314f6bdf3b672147684e0fb515de7c508899
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-02-09 09:36:32 +0530

    Golang - How to generate random shuffle of array
    we have used rand.NewSource and time.now unixNano to generate int64
    and with that input we genarated random source and with that rand we genrated random index position
    
    https://golang.org/pkg/time/#Time.UnixNano
    https://golang.org/pkg/math/rand/

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	cards := newDeck()
 
-	cards.saveToFile("fileName.txt")
-	s := newDeckFromFile("fileName.txt")
-	// fmt.Println("The values are ",s)
-	fmt.Println()
-	s.print()
+	cards.shuffle()
+	cards.print()
 
 }
