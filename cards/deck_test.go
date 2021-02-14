9d7bc4c0cf518283ac554f7e217de867bf458c68
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-02-14 23:28:22 +0530

    Added file tests read and delte file

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // this is for testing
 func TestNewDeck(t *testing.T) {
@@ -9,4 +12,21 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expecting 16 but got %v ", len(d))
 	}
+
+	if d[len(d)-1] != "four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadDeck := newDeckFromFile("_decktesting")
+	if len(loadDeck) != 16 {
+		t.Errorf("Expecting 16 but got %v ", len(deck))
+	}
+	os.Remove("_decktesting")
+
 }
