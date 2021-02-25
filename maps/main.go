609536208f577ba0f84e5ddede6d07b79e0321e8
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-02-25 23:29:31 +0530

    Added interface in golang

--- maps/main.go
+++ maps/main.go
@@ -15,13 +15,14 @@ func main() {
 
 	//this statement will get ignored
 	delete(nattukozhi, "akisfs")
-
-	// fmt.Println(nattukozhi)
 	printMap(nattukozhi)
+	fmt.Println(nattukozhi)
+
 }
 
 func printMap(c map[string]string) {
 	for kozhi, days := range c {
 		fmt.Println(kozhi, " is ", days)
 	}
+	c["aki"] = "6days"
 }
