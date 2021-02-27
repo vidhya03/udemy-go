54e962d11e59a5f1a571611c47f58c1be4b8ee1c
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-02-27 11:17:52 +0530

    bot greeting interface in golang

--- interface/main.go
+++ interface/main.go
@@ -7,13 +7,17 @@ type bot interface {
 }
 type englishBot struct{}
 type spanishBot struct{}
+type deck string
 
 func main() {
 
 	eb := englishBot{}
 	sb := spanishBot{}
+	var s deck = "vde"
+
 	printGreeting(eb)
 	printGreeting(sb)
+	printGreeting(s)
 
 }
 func (eb englishBot) getGreeting() string {
@@ -23,6 +27,9 @@ func (eb englishBot) getGreeting() string {
 
 func printGreeting(b bot) {
 	fmt.Println(b.getGreeting())
+}
+func (ss deck) getGreeting() string {
+	return "deck string"
 }
 
 func (eb spanishBot) getGreeting() string {
