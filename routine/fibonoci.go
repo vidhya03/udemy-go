96a198be4f45055a003158d937ef715bc688ffe0
Vidhyadharan, Deivamani <it.vidhyadharan@gmail.com>
2021-03-16 23:57:45 +0530

    Understanding fibonoci concurrency

--- routine/fibonoci.go
+++ routine/fibonoci.go
@@ -3,19 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(fib(5))
+	input := 5
+	fmt.Println("Input of, ", input, " is ", fib(input))
+
+	len := 45
+	jobs := make(chan int, len)
+	results := make(chan int, len)
+
+	go worker(jobs, results)
+	// go worker(jobs, results)
+	// go worker(jobs, results)
+	// go worker(jobs, results)
+	// go worker(jobs, results)
+	// go worker(jobs, results)
+
+	for i := 0; i < len; i++ {
+		jobs <- i
+	}
+	close(jobs)
+	for j := 0; j < len; j++ {
+		fmt.Println(<-results)
+	}
+
 }
 
-func worker(jobs <-chan int, results <-chan int) {
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
 
 }
 
 func fib(n int) int {
-	if n == 1 {
-		return n
-	} else if n <= 0 {
+	if n <= 0 {
 		return 0
+	} else if n == 1 {
+		return 1
 	}
-	return fib(n-1) + fib(n-2)
+	return fib(n-1) + fib(n-2) + 1
 
 }
